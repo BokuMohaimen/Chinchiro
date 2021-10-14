7c4a9fb5c3a594205f8c16f2ee431140575ee043
Mohaimen_Ksr <61966215+BokuMohaimen@users.noreply.github.com>
2021-10-14 20:40:21 +0900

    first

---
+++ Chinchirorin.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int money1, money2;
+char com[5];
+
+int main(void) {
+
+	do {
+
+		help();
+
+		if (com == "help") {
+
+			printf("-----チンチロコマンド一覧-----\n");
+			printf("help / このページを表示します\n");
+			printf("rule / ルールの一覧を表示します\n");
+			printf("new / 新しくゲームを開始します\n");
+			printf("stop / ゲームを終了します\n");
+
+		}
+
+		else if (com == "rule") {
+
+			printf("====================\n");
+			printf("役一覧: [1:1:1 jackpotチャンス] [それ以外の三つ揃い ゾロメ]\n");
+			printf("[出目合計10 man10] [1・2・3 イチ・ニ・サン・ﾀﾞｰ!!] [出目合計5 dan5]\n");
+			printf("役一覧: [二つそろって残りが・・ その数字が強さ]\n\n");
+			printf("配当率: 『サイコー!!:4倍勝(親のみ)』\n");
+			printf("配当率: 『イチ・ニ・サン・ﾀﾞｰ!! 2.5倍勝』 『ゾロメ:3倍勝』\n");
+			printf("『man10:2倍勝』『dan5:2倍負』 通常:1倍負/勝\n");
+
+		}
+
+	} while (com == "stop");
+
+	return 0;
+
+}
+
+int help() {
+
+	com[0] = '\0';
+	printf("チンチロリンへようこそ:)\n");
+	printf("helpと入力するとコマンド一覧が見れます！\n");
+	printf("command:");
+	scanf("%s", com);
+
+}
